ffd2b835efe9c8e54127dbc3a84b02b33eb00dc1
Jovani Oliveira Brasil <jovanibrasil@gmail.com>
2020-04-11 02:48:05 -0300

    Adicionar configuração de build do travis e corrige testes

--- src/test/java/com/blog/repositories/UserRepositoryTest.java
+++ src/test/java/com/blog/repositories/UserRepositoryTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -22,6 +23,7 @@ import static org.junit.Assert.assertNotNull;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class UserRepositoryTest {
 
 	@Autowired
